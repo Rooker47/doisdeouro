d9ccd42b665103475052760b0185babdc7f1cdb1
Celso Silva <cjrti95@gmail.com>
2020-09-01 14:24:44 -0300

    01/09/2020-14h24  -  Acesso ao "admin" corrigido

--- doisdeouro/urls.py
+++ doisdeouro/urls.py
@@ -4,7 +4,6 @@ from .views import IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path(r'^admin_tools/', include('admin_tools.urls')),
     path('admin/', admin.site.urls),
     path('cadastrar/', include('appCadastros.urls'), name='cadastrar'),
     path('listar/', include('appListas.urls'), name='listar'),
