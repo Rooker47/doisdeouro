e72feb495a1b896d05f2f5f8ad80691a575ad305
Celso Silva <cjrti95@gmail.com>
2020-09-02 22:37:08 -0300

    02/09/2020  -  *Estável  -  **Problema no "admin"

--- appUsuarios/models.py
+++ appUsuarios/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Perfil(models.Model):
-    nomeCompletoUsuario = models.CharField(max_length=100, null=True)
-    matriculaUsuario = models.IntegerField(null=True)
-    telefoneUsuario = models.CharField(max_length=16, null=True)
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    nomeCompletoUsuario = models.CharField(max_length=100, null=True, verbose_name="nome completo")
+    matriculaUsuario = models.IntegerField(null=True, verbose_name="matrícula")
+    telefoneUsuario = models.CharField(max_length=16, null=True, verbose_name="telefone")
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="nome de usuário")
 
     def __str__(self):
         return "{}".format(self.usuario)
