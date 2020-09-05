70d3ed0372549efb24477d8e87cafa02fd970496
Celso Silva <cjrti95@gmail.com>
2020-09-05 16:54:02 -0300

    criação da appCore

--- doisdeouro/settings.py
+++ doisdeouro/settings.py
@@ -38,11 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 # MINHAS APPs =========================================================================================================
-    'appDelete.apps.AppdeleteConfig',
     'appCreate.apps.AppcreateConfig',
+    'appCore.apps.AppcoreConfig',
+    'appDelete.apps.AppdeleteConfig',
+    'appListas.apps.ApplistasConfig',
     'appUpdate.apps.AppupdateConfig',
     'appUsuarios.apps.AppusuariosConfig',
-    'appListas.apps.ApplistasConfig',
 
 # APPs de plugins =====================================================================================================
     'daterange_filter',
